5c8a9af6fb56bfbdd9efea39701262ffed444698
ruzaigh <ruzaigh.kalam2@gmail.com>
2022-12-06 17:50:22 +0200

    busy with it

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.magic8ball.app',
+  appName: '8ballApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
